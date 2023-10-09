9552bced47d4179bf8e2e0407f4d912d0e8e99db
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-10-09 19:35:28 +0100

    Add section 1

---
+++ 1_bash/1_bash.sh
@@ -0,0 +1,14 @@
+whoami
+pwd # print working directory
+
+# / is the root directory
+# /bin is  where built in programs are stored
+# /data is for misc data files 
+# /users is where users' personal files are stored 
+# /tmp is for temporary files that don't need to be stored
+
+#wget https://www.uhpc-training.co.uk/material/technology_and_tooling/bash_shell/shell-novice.zip
+
+ls -F ./shell-novice # -F flag adds / to end of directories
+
+ls -F ./shell-novice/shell/test_directory
